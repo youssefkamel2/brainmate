ad94e93675202e5acc123930ec0c172fd75366cc
youssef kamel <youssefsalem383@gmail.com>
2024-11-03 23:47:40 +0200

    first

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,11 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // Return null for API requests to avoid redirect
+        if ($request->is('api/*')) {
+            return null; // No redirect for API routes
+        }
+
+        return route('login');
     }
 }
