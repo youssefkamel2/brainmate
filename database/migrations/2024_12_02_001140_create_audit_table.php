1f64d79ae677fb31a24d82b28a58e098f6ba0ea6
youssef kamel <youssefsalem383@gmail.com>
2025-01-04 03:24:14 +0200

    Add Dockerfile deployment

--- database/migrations/2024_12_02_001140_create_audit_table.php
+++ database/migrations/2024_12_02_001140_create_audit_table.php
@@ -13,8 +13,8 @@ class CreateAuditTable extends Migration
             $table->string('table_name'); // The table that was modified
             $table->bigInteger('record_id'); // ID of the record that was modified
             $table->text('action'); // Type of action (create, update, delete)
-            $table->json('old_values')->nullable(); // Previous values (before the change)
-            $table->json('new_values')->nullable(); // New values (after the change)
+            $table->text('old_values')->nullable(); // Previous values (before the change)
+            $table->text('new_values')->nullable(); // New values (after the change)
             $table->foreignId('user_id')->nullable()->constrained('users')->onDelete('set null'); // User who made the change
             $table->timestamps();
         });
