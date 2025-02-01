882776dc9373a30da7ea9fe2dceef8495e67756f
Bkoura-eng <youssefsalem383@gmail.com>
2025-02-01 19:19:36 +0200

    test

--- app/Http/Controllers/ModelTestController.php
+++ app/Http/Controllers/ModelTestController.php
@@ -2,30 +2,31 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Task;
-use App\Models\Team;
-use App\Models\User;
-use App\Models\Project;
-use App\Models\Reminder;
-use App\Models\TaskNote;
-use App\Models\Workspace;
-use App\Models\Attachment;
-use App\Models\TaskMember;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
-use App\Traits\ResponseTrait;
+use Illuminate\Support\Facades\Log;
 
 class ModelTestController extends Controller
 {
+    public function testModels(Request $request)
+    {
+        // Fetching environment variables to check if they are properly set
+        $envVariables = [
+            'APP_ENV' => env('APP_ENV', 'no'),
+            'DB_CONNECTION' => env('DB_CONNECTION', 'no conn'),
+            'DB_HOST' => env('DB_HOST', 'nohost'),
+            'DB_PORT' => env('DB_PORT', 'noDB_PORT'),
+            'DB_DATABASE' => env('DB_DATABASE', 'nodb'),
+            'DB_USERNAME' => env('DB_USERNAME', 'nous'),
+            'DB_PASSWORD' => env('DB_PASSWORD', 'nopw'),
+            'DB_CHARSET' => env('DB_CHARSET', 'noch'),
+            'DB_COLLATION' => env('DB_COLLATION', 'noco'),
 
-    use ResponseTrait;
+        ];
 
-    public function testModels(Request $request){
+        // Log the environment variables to check in logs
+        Log::info('Environment Variables:', $envVariables);
 
-        $test = env('DB_DATABASE','no');
-
-        return $this->success($test, 200);
-
-
+        // Return the environment variables for testing
+        return response()->json($envVariables, 200);
     }
 }
